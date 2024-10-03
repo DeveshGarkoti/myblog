56e4b9c45fe389ab32154188e89f00660c526686
DeveshGarkoti <deveshgar280@gmail.com>
2024-10-02 17:15:01 -0700

    static files

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+
+# Base URL to serve static files
+STATIC_URL = '/static/'
+
+# Directory where static files will be collected (for production)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Directories where Django will look for static files (for development)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Replace with your actual static folder
+]
+
